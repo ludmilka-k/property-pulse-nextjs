6a0dabac2b10758dd2dcc36a1935bc90ddf8f206
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2024-10-28 13:31:59 +0200

    Get user and Submit to database

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,6 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utiles/getSessionUser"
 
 // GET  /api/properties
 export const GET = async (request) => {
@@ -17,6 +18,16 @@ export const GET = async (request) => {
 // POST  /api/properties
 export const POST = async (request) => {
     try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+
+        if (!sessionUser || !sessionUser.userId) {
+            return new Response("User ID is required", {status: 401});
+        }
+
+        const { userId } = sessionUser;
+
         const formData = await request.formData();
 
         // Access all values from amenities and images
@@ -49,11 +60,16 @@ export const POST = async (request) => {
                 email: formData.get("seller_info.email"),
                 phone: formData.get("seller_info.phone"),
             },
-            images
-        }
-        console.log(propertyData);
+            owner: userId,
+            // images
+        };
 
-        return new Response(JSON.stringify({message: "Success"}), {status: 200});
+        const newProperty = new Property(propertyData);
+        await newProperty.save();
+
+        return Response.redirect(`${process.env.NEXTAUTH_URL}/properties/${newProperty._id}`);
+
+        // return new Response(JSON.stringify({message: "Success"}), {status: 200});
     } catch (error) {
         return new Response("Filed to add property!", {status: 500});
     }
