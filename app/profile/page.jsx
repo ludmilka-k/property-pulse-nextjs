71a318db5d1db5e6f82410fc88604d12e63a661f
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2024-10-31 16:12:44 +0200

    Delete property

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -40,7 +40,28 @@ const ProfilePage = () => {
             fetchUserProperties(session.user.id);
     }, [session])
 
-    const handleDeleteProperty = () => {}
+    const handleDeleteProperty = async (propertyId) => {
+        const confirmed = window.confirm("Are you sure you want to delete this property?");
+
+        if (!confirmed) return;
+        
+        try {
+            const res = await fetch(`/api/properties/${propertyId}`, {method: "DELETE"});
+
+            if (res.status === 200) {
+                // Remove property from state
+                const updatedProperty = properties.filter((property) => property._id !== propertyId);
+                setProperties(updatedProperty);
+
+                alert("Property Deleted")
+            } else {
+                alert("Failed to delete this property");
+            }
+        } catch (error) {
+            console.log(error)
+            alert("Failed to delete this property");
+        }
+    }
 
     return (
         <section className="bg-blue-50">
@@ -83,8 +104,9 @@ const ProfilePage = () => {
                                             <Image
                                                 className="h-32 w-full rounded-md object-cover"
                                                 src={property.images[0]}
-                                                width={500}
-                                                height={100}
+                                                width={0}
+                                                height={0}
+                                                sizes="100vw"
                                                 priority={true}
                                                 alt=""
                                             />
