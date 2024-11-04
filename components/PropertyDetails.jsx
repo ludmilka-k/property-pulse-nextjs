ba364bbf1815f26db7f303382146fb14e1430b0a
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2024-11-04 11:09:06 +0200

    Property map using geocoding and mapbox

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,4 +1,5 @@
 import {  FaBed, FaBath, FaRulerCombined, FaTimes, FaCheck, FaMapMarker } from "react-icons/fa";
+import PropertyMap from "@/components/PropertyMap";
 
 const PropertyDetails = ({ property }) => {
     return (
@@ -96,7 +97,7 @@ const PropertyDetails = ({ property }) => {
             </div>
             {/*<!-- Map -->*/}
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-                <div id="map"></div>
+                <PropertyMap property={property}/>
             </div>
         </main>
     )
