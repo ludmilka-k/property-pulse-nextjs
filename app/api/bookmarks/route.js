dfa87660502acdb3c5429e11fe94a60c9a96e34c
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2024-11-05 11:27:00 +0200

    Saved properties

--- app/api/bookmarks/route.js
+++ app/api/bookmarks/route.js
@@ -1,8 +1,35 @@
 import connectDB from "@/config/database";
 import User from "@/models/User";
+import Property from "@/models/Property";
 import { getSessionUser } from "@/utiles/getSessionUser";
 
 export const dynamic = "force-dynamic";
+
+// GET /api/bookmarks
+export const GET = async() => {
+    try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+
+        if (!sessionUser || !sessionUser.userId) {
+            return new Response("User ID is required", {status: 401});
+        }
+
+        const { userId } = sessionUser;
+
+        // Find user in database
+        const user = await User.findById(userId);
+
+        // Get user bookmarks
+        const bookmarks = await Property.find({_id: {$in: user.bookmarks}});
+
+        return new Response(JSON.stringify(bookmarks), {status: 200});
+    } catch (error) {
+        console.log(error);
+        return new Response("Something Went Wrong!", {status: 500});
+    }
+};
 
 // POST  /api/bookmarks
 export const POST = async (request) => {
