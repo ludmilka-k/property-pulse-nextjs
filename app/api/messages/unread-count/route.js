bb73a2ce887ba3cd1edc002715561240077931ec
Ludmila Kurochkina <ludmilka.kurochkina@gmail.com>
2024-11-11 23:03:48 +0200

    Unread message count

---
+++ app/api/messages/unread-count/route.js
@@ -0,0 +1,30 @@
+import connectDB from "@/config/database";
+import Message from "@/models/Message";
+import { getSessionUser} from "@/utiles/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+// GET  /api/messages/unread-count
+export const GET = async (request) => {
+    try {
+        await connectDB();
+
+        const sessionUser = await getSessionUser();
+        if (!sessionUser || !sessionUser.user) {
+            return new Response("User ID is required", {status: 401});
+        }
+
+        const { userId } = sessionUser;
+
+       const count= await Message.countDocuments({
+           recipient: userId,
+           read: false
+       })
+
+        return new Response(JSON.stringify(count), {status: 200});
+
+    } catch (error) {
+        console.log(error);
+        return new Response("Something Went Wrong!", {status: 500});
+    }
+};
